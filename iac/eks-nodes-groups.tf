4306721a5fd4ac97f7fb7a83e22197001a7a9a88
onyeoka <chinedu.onyeoka@gmail.com>
2023-01-20 18:33:59 +0100

    wip

---
+++ iac/eks-nodes-groups.tf
@@ -0,0 +1,148 @@
+# Resource: aws_iam_role
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+
+# Create IAM role for EKS Node Group
+resource "aws_iam_role" "nodes_general" {
+  # The name of the role
+  name = var.eks_node_group_name
+
+  # The policy that grants an entity permission to assume the role.
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }, 
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+# Resource: aws_iam_role_policy_attachment
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy_general" {
+  # The ARN of the policy you want to apply.
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKSWorkerNodePolicy
+  policy_arn = var.eks_iam_policy_attachment  
+
+  # The role the policy should be applied to
+  role = aws_iam_role.nodes_general.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy_general" {
+  # The ARN of the policy you want to apply.
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKS_CNI_Policy
+  policy_arn = var.eks_iam_cni_policy
+
+  # The role the policy should be applied to
+  role = aws_iam_role.nodes_general.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
+  # The ARN of the policy you want to apply.
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEC2ContainerRegistryReadOnly
+  policy_arn = var.amazon_ec2_container_registry_read_only
+
+  # The role the policy should be applied to
+  role = aws_iam_role.nodes_general.name
+}
+
+# Resource: aws_eks_node_group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_node_group
+
+
+resource "aws_eks_node_group" "nodes_general" {
+  # Name of the EKS Cluster.
+  cluster_name = aws_eks_cluster.eks.name
+
+  # Name of the EKS Node Group.
+  node_group_name = var.eks_node_group_name
+
+  # Amazon Resource Name (ARN) of the IAM Role that provides permissions for the EKS Node Group.
+  node_role_arn = aws_iam_role.nodes_general.arn
+
+  # Identifiers of EC2 Subnets to associate with the EKS Node Group. 
+  # These subnets must have the following resource tag: kubernetes.io/cluster/CLUSTER_NAME 
+  # (where CLUSTER_NAME is replaced with the name of the EKS Cluster).
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+    aws_subnet.private_3.id
+  ]
+
+  provisioner "local-exec" {
+    working_dir = "./ansible"
+    command = "ansible-playbook deploy-microservices.yml --extra-vars='ACCOUNT_ID=$ACCOUNT_ID CLUSTER_NAME=$CLUSTER_NAME'"
+    environment = {
+      ACCOUNT_ID = var.account_id
+      CLUSTER_NAME = var.eks_cluster
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "echo -n $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text) > instance"
+  }
+
+
+  
+
+
+  # Configuration block with scaling settings
+  scaling_config {
+    # Desired number of worker nodes.
+    desired_size = 1
+
+    # Maximum number of worker nodes.
+    max_size = 1
+
+    # Minimum number of worker nodes.
+    min_size = 1
+  }
+  # allowing remote connection
+  # remote_access {
+  #   ec2_ssh_key = "bashir"
+  #   source_security_group_ids = [ aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id ]
+  # }
+
+  launch_template {
+    id = aws_launch_template.eks-nodes-launch.id
+    version = aws_launch_template.eks-nodes-launch.latest_version
+  }
+  # Type of Amazon Machine Image (AMI) associated with the EKS Node Group.
+  # Valid values: AL2_x86_64, AL2_x86_64_GPU, AL2_ARM_64
+  ami_type = var.eks_ami_type
+
+  # Type of capacity associated with the EKS Node Group. 
+  # Valid values: ON_DEMAND, SPOT
+  capacity_type = var.eks_capacity_type
+
+  # Disk size in GiB for worker nodes
+  # disk_size = 20
+
+  # Force version update if existing pods are unable to be drained due to a pod disruption budget issue.
+  force_update_version = false
+
+  # List of instance types associated with the EKS Node Group
+  # instance_types = ["t3.small"]
+
+  labels = {
+    role = var.eks_node_group_name
+  }
+
+  # Kubernetes version
+  version = var.eks_version
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy_general,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy_general,
+    aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only,
+  ]
+}

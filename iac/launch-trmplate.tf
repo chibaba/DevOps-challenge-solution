0905a47191d4a5a1b8c28f9c25f5e0f739e42b9c
onyeoka <chinedu.onyeoka@gmail.com>
2023-01-20 18:25:02 +0100

    tpl

---
+++ iac/launch-trmplate.tf
@@ -0,0 +1,13 @@
+# Launch template for the worker nodes
+resource "aws_launch_template" "eks-nodes-launch" {
+  name = "boo"
+
+  instance_type = var.eks_instance_type
+  key_name = var.key_name
+  
+
+  vpc_security_group_ids = [aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id]
+
+
+  user_data = filebase64("script.sh")
+}
